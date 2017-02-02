129dc7241811bf884fecd0a06ff086dbf4158fe0
fencerValery <val-klimovch.yandex.ru>
2017-02-02 16:50:41 +0300

    add method factorial

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
 public class Hello{
-  public static void main(string[] args){
+  public static int factorial(int x){
+    if (x == 1){
+       return x;
+    }
+       return factorial(x - 1) * x;  
+  }
+  public static void main(String[] args){
     System.out.print("Hello!");
-
+    System.out.print("\nWorld!");
+    System.out.print("\nFactorial = " + factorial(12));
   }
 }
