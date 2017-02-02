4075ea719fc0accef5361df4d2753ac79740ee24
fencerValery <val-klimovch.yandex.ru>
2017-02-02 16:14:32 +0300

    My second commit from notebook

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
   public static void main(string[] args){
     System.out.print("Hello!");
     System.out.print(" world!");
+    String s = "Hello word!");
+    System.out.print("\n" + s + "\nfrom notebook");
 
   }
 }
